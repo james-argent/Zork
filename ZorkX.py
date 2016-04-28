34e3fb7ce7646339050daacde1918fc4bf848438
James Argent <james-argent@softwareimaging.com>
2016-04-28 13:50:03 +0100

    Added location identification function. Fixed issue with north/south/east/west. Minor tweeks

--- ZorkX.py
+++ ZorkX.py
@@ -10,12 +10,23 @@ nullMessage = "Looks like you forgot to say anything"
 fullStop = "."
 defaultUserName = "Jon Snow"
 unknownCommand = "That command was unknown. Please enter another."
+unknownLocation = "This location doesn't have a name."
 idleMessage = "What would you like to do now?\n"
 userNameWrong = nullMessage + ", I will just call you " + defaultUserName + fullStop
 null = ""
 directions = ["north", "east", "south", "west"]
 successMove = "You travelled "
-horiVertiHoriVerti = [0, 1, 0, 1] #horizontal/vertical swap
+horiVerti = [1, 0, 1, 0] #horizontal/vertical
+
+#location tuples
+location1 = (0, 0, "Castle Black")
+location2 = (1, 0, "Eastwatch-by-the-Sea")
+location3 = (-1, 0, "The Shadow Tower")
+location4 = (0, 1, "Crastor's Keep")
+location5 = (0, 2, "The Fist of the First Men")
+location6 = (1, 2, "Hardhome")
+
+tuplesList = location1, location2, location3, location4, location5, location6
 
 #subject to change
 location = [0,0]
@@ -33,6 +44,16 @@ def checkInput(thingToCheck, currentInventory, currentHealth):
         if (knownCommands[i] in thingToCheck):
             print (knownMessages[i] + str(knownCurrent[i]))
 
+def identifyLocation(location):
+    found = False
+    for i in range (0, len(tuplesList)):
+        if location[0] == (tuplesList[i])[0] and location[1] == (tuplesList[i])[1]:
+            print (locationMessage + (tuplesList[i])[2])
+            found = True
+    if found == False:
+        print (locationMessage + str(location))
+        print (unknownLocation)
+
 #if the user doesn't enter a name, they're given a default name
 userName = input(nameRequest)
 if (len(userName) < 1):
@@ -49,10 +70,11 @@ while health > 0:
     replies = 0
 
     #update the user on their situation
-    print (locationMessage + str(location))
+    identifyLocation(location)
 
     #ask for next action
     command = input(idleMessage)
+    command = command.lower()
 
     if command == null:
         print (nullMessage + fullStop)
@@ -75,9 +97,9 @@ while health > 0:
     for i in range (0, (len(directions))):
         if (directions[i] in command):
             if (i <= 1):
-                location[horiVertiHoriVerti[i]] += 1
+                location[horiVerti[i]] += 1
             else:
-                location[horiVertiHoriVerti[i]] -= 1
+                location[horiVerti[i]] -= 1
             replies += 1
             print (successMove + directions[i] + fullStop)
     
