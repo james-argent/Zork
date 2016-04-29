c9352fa31c4257632160fbe74b4332b747fc3f85
James Argent <james-argent@softwareimaging.com>
2016-04-29 09:58:01 +0100

    Added the ability to add items to your inventory based upon your location. Added the max size of your inventory

--- ZorkX.py
+++ ZorkX.py
@@ -8,34 +8,38 @@ quitMessage = "If you would like to quit, just close the console."
 usedMessage = "You used your "
 nullMessage = "Looks like you forgot to say anything"
 fullStop = "."
+takeMessage1 = "You put the "
+takeMessage2 = " into your inventory."
 deadMessage = "You died."
+inventoryFullMessage = "Your inventory is too full!"
 defaultUserName = "Jon Snow"
 unknownCommand = "That command was unknown. Please enter another."
 unknownLocation = "This location doesn't have a name."
 idleMessage = "What would you like to do now?\n"
 userNameWrong = nullMessage + ", I will just call you " + defaultUserName + fullStop
 null = ""
-directions = ["north", "east", "south", "west"]
+directions = ("north", "east", "south", "west")
 successMove = "You travelled "
-horiVerti = [1, 0, 1, 0] #horizontal/vertical
+horiVerti = (1, 0, 1, 0) #horizontal/vertical
 
 #location tuples (x-coordinate, y-coordinate, location name, location comment)
-loc1 = (0, 0, "Castle Black", "You're home, but something's not right, Ser Alliser Thorne seems to be plotting something...")
-loc2 = (1, 0, "Eastwatch-by-the-Sea", "One of the only manned castles along the wall. The castle furthest East.")
-loc3 = (-1, 0, "The Shadow Tower", "One of the three remaining manned castles along the wall.")
-loc4 = (0, 1, "Crastor's Keep", "Keep your hands off of his daughter wives or he'll kill you.")
-loc5 = (0, 2, "The Fist of the First Men", "A big battle happened here, but where are the dead brothers?")
-loc6 = (1, 2, "Hardhome", "The wildings have a settlement here. Better not disturb them.")
-loc7 = (0, -1, "Moletown", "Known for its attractive qualities.")
-loc8 = (0, -2, "Winterfell", "My old home. The north remembers.")
-loc9 = (1, -2, "The Dreadfort", "The seat of the traitorous House Bolton.")
-loc10 = (0, -3, "Moat Cailin", "An important location by which to control the Neck.")
-tuplesList = [loc1, loc2, loc3, loc4, loc5, loc6, loc7, loc8, loc9, loc10]
+loc1 = [0, 0, "Castle Black", "You're home, but something's not right, Ser Alliser Thorne seems to be plotting something...",""]
+loc2 = [1, 0, "Eastwatch-by-the-Sea", "One of the only manned castles along the wall. The castle furthest East.",""]
+loc3 = [-1, 0, "The Shadow Tower", "One of the three remaining manned castles along the wall.",""]
+loc4 = [0, 1, "Crastor's Keep", "Keep your hands off of his daughter wives or he'll kill you. There's some tasty pork roasting over the fire.","pork"]
+loc5 = [0, 2, "The Fist of the First Men", "A big battle happened here, but where are the dead brothers?",""]
+loc6 = [1, 2, "Hardhome", "The wildings have a settlement here. Better not disturb them.",""]
+loc7 = [0, -1, "Moletown", "Known for its attractive qualities.",""]
+loc8 = [0, -2, "Winterfell", "My old home. The north remembers.",""]
+loc9 = [1, -2, "The Dreadfort", "The seat of the traitorous House Bolton.",""]
+loc10 = [0, -3, "Moat Cailin", "An important location by which to control the Neck.",""]
+locationsList = [loc1, loc2, loc3, loc4, loc5, loc6, loc7, loc8, loc9, loc10]
 
 #information about your character which is subject to change
 location = [0,0] # Castle Black
 health = 10
 inventory = ["wine","mutton"]
+currentLocationName = ""
 
 #arrays which must be the same length
 knownCommands = ["inventory", "health", "quit"]
@@ -51,11 +55,13 @@ def checkInput(thingToCheck):
 #tells the user where they are or just gives the coordinates if the place has no recorded name
 def identifyLocation(location):
     found = False
-    for i in range (len(tuplesList)):
-        if location[0] == (tuplesList[i])[0] and location[1] == (tuplesList[i])[1]:
-            print (locationMessage + (tuplesList[i])[2])
-            print ((tuplesList[i])[3])
+    for i in range (len(locationsList)):
+        if location[0] == (locationsList[i])[0] and location[1] == (locationsList[i])[1]:
+            print (locationMessage + (locationsList[i])[2])
+            print ((locationsList[i])[3])
             found = True
+            global currentLocationName
+            currentLocationName = (locationsList[i])[2]
     if found == False:
         print (locationMessage + str(location))
         print (unknownLocation)
@@ -74,6 +80,7 @@ print (healthMessage + str(health))
 #keep the game going as long as the user is 'alive'
 while health > 0:
     replies = 0
+    inventoryAction = False
 
     #update the user on their situation
     identifyLocation(location)
@@ -90,14 +97,27 @@ while health > 0:
         if ((knownCommands[i]) in command):
             checkInput(command)
             replies += 1
-    
+ 
+    #picking up an item
+    for i in range (len(locationsList)):
+        if (currentLocationName == (locationsList[i])[2]):
+            if (((locationsList[i])[4]) in command) and ((locationsList[i])[4] is not null):
+                if (len(inventory)) < 7:
+                    inventory.append((locationsList[i])[4])
+                    print (takeMessage1 + (locationsList[i])[4] + takeMessage2)
+                else:
+                    print (inventoryFullMessage)
+                replies += 1
+                inventoryAction = True
+               
     #using up an item in the inventory
-    for item in inventory:
-        if item in command:
-            print (usedMessage + item + fullStop)
-            inventory.remove(item)
-            replies += 1
-            break
+    if inventoryAction == False:
+        for item in inventory:
+            if item in command:
+                print (usedMessage + item + fullStop)
+                inventory.remove(item)
+                replies += 1
+                break
 
     #deals with movement
     for i in range (len(directions)):
